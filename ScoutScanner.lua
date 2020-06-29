78f568487775660be58f2c5123566f7616d058d6
Henrik Tufte Lien <tuftelien@users.noreply.github.com>
2020-06-29 06:39:35 +0200

    Scoutscanner improvements (#1)
    Improve scout scanner

--- ScoutScanner.lua
+++ ScoutScanner.lua
@@ -1,12 +1,10 @@
 WBCoalition.ScoutScanner = {}
-
 local Scanner = WBCoalition.ScoutScanner
 
-local ENEMIES = {{'Praxis', 'Legacy', 'A Bards Tale'}}
-
-local WBC
-
-local Scanner = WBCoalition.ScoutScanner
+local competitors = {
+    {'Praxis', 'Undying Legacy', 'A Bards Tale', 'Neighbourhood'},
+    {'Redrum Inc', 'RR Inc', 'Solid', 'CORE', 'Ember', 'Close to Insanity', 'Scarlet Crusade'}
+}
 
 local isScanning = false
 
@@ -15,7 +13,7 @@ local ZONES = {'Azshara', 'Blasted Lands', 'Ashenvale', 'Feralas', 'Duskwood', '
 local results = {}
 local zoneIndex = 0
 local gotResultsFor = 0
-local try = 0
+local try = 1
 
 local DIALOG_CONTINUE_SCANNING = 'DIALOG_WBC_CONTINUE_SCANNING'
 
@@ -26,11 +24,11 @@ local function isScout(name)
     return false
 end
 
-local function isEnemyCoalition(name,guild)
+local function isCompetitor(name,guild)
     if name == 'Kimeera' then return 1 end
-    for i=1,#ENEMIES do
-        for j=1,#ENEMIES[i] do
-            if ENEMIES[i][j] == guild then
+    for i=1,#competitors do
+        for j=1,#competitors[i] do
+            if competitors[i][j] == guild then
                 return i
             end
         end
@@ -45,38 +43,45 @@ local function reportPlayerInfo(index, playerInfo)
     if playerInfo.level < 60 then
         levelColor = ' |cff33aa33'
     end
-    WBC:Log('        ' .. index .. '. ' .. ' |c' .. classColor .. playerInfo.name  .. levelColor.. playerInfo.level .. '|cffffffff, ' .. playerInfo.guild)
+    WBCoalition:Log('        ' .. index .. '. ' .. ' |c' .. classColor .. playerInfo.name  .. levelColor.. playerInfo.level .. '|cffffffff, ' .. playerInfo.guild)
 end
 
 local function reportResults()
-    WBC:Log('-- RESULTS --')
+    WBCoalition:Log('Scout Scan Results: ')
     for i=1,#results do
         local zoneInfo = results[i]
-        WBC:Log('|cffffffcc' .. zoneInfo.name)
-        WBC:Log('   Coalition:')
-        for j=1,#zoneInfo.scouts do
-            local scout = zoneInfo.scouts[j]
-            reportPlayerInfo(j, scout)
+        WBCoalition:Log('|cffffffcc' .. zoneInfo.name)
+
+        if #zoneInfo.scouts > 0 then
+            WBCoalition:Log('   Coalition:')
+            for j=1,#zoneInfo.scouts do
+                local scout = zoneInfo.scouts[j]
+                reportPlayerInfo(j, scout)
+            end
+        else
+            WBCoalition:Log('   |cffff0000We are missing scouts in ' .. zoneInfo.name .. '!')
         end
-        if #zoneInfo.scouts == 0 then
-            WBC:Log('   |cffff0000We are missing scouts in ' .. zoneInfo.name .. '!!')
-        end
-        WBC:Log('   Praxis Coalition:')
-        for j=1,#zoneInfo.enemies[1] do
-            local scout = zoneInfo.enemies[1][j]
-            reportPlayerInfo(j, scout)
-        end
-        if #zoneInfo.enemies[1] == 0 then
-            WBC:Log('   |cffff0000Praxis is missing scouts in ' .. zoneInfo.name .. '!!')
+
+        for c=1,#competitors do
+            local competitorLeader = competitors[c][1]
+            if #zoneInfo.competitors[c] > 0 then
+                WBCoalition:Log('   ' .. competitorLeader ..':')
+                for j=1,#zoneInfo.competitors[c] do
+                    local scout = zoneInfo.competitors[c][j]
+                    reportPlayerInfo(j, scout)
+                end
+            else
+                WBCoalition:Log('   |cffff0000' .. competitorLeader .. ' is missing scouts in ' .. zoneInfo.name .. '!')
+            end
         end
     end
 end
 
 local function saveWhoResults()
     local x, total = C_FriendList.GetNumWhoResults()
-    local zoneInfo = {name = ZONES[zoneIndex], scouts={}, enemies={}, total = total}
-    for i=1,#ENEMIES do
-        table.insert(zoneInfo.enemies, {})
+    local zoneInfo = {name = ZONES[zoneIndex], scouts={}, competitors={}, total = total}
+    for i=1,#competitors do
+        table.insert(zoneInfo.competitors, {})
     end
     for i=1,total do
         local info = C_FriendList.GetWhoInfo(i)
@@ -93,9 +98,9 @@ local function saveWhoResults()
         if isScout(name) then
             table.insert(zoneInfo.scouts, playerInfo)
         end
-        local enemyCoalition = isEnemyCoalition(name, guild)
-        if enemyCoalition > 0 then
-            table.insert(zoneInfo.enemies[enemyCoalition], playerInfo)
+        local competitor = isCompetitor(name, guild)
+        if competitor > 0 then
+            table.insert(zoneInfo.competitors[competitor], playerInfo)
         end
     end
     table.insert(results, zoneInfo)
@@ -103,34 +108,42 @@ end
 
 local function scanNextZone()
     StaticPopup_Hide(DIALOG_CONTINUE_SCANNING)
+
+    if zoneIndex > #ZONES then return end
+
     local zone = ZONES[zoneIndex]
-    local msg = ''
+    local msg = zone
     if try > 1 then
-        msg = 'Scan failed\n'
+        msg = msg .. ' (attempt ' .. try .. ')'
     end
-    msg = msg .. 'Scanning ' .. zone
-    msg = msg .. '\n' .. zoneIndex .. '/' .. #ZONES 
+    msg = msg .. '\n' .. zoneIndex .. '/' .. #ZONES
 
     StaticPopup_Show(DIALOG_CONTINUE_SCANNING, msg)
 end
 
 StaticPopupDialogs[DIALOG_CONTINUE_SCANNING] = {
     text = '%s',
-    button1 = 'Continue scanning',
-    button2 = 'Cancel',
+    button1 = 'Scan',
+    button2 = 'Stop',
     OnAccept = function(self, data)
-        local currentIndex = zoneIndex
         local zone = ZONES[zoneIndex]
+        C_FriendList.SetWhoToUi(true)
+        FriendsFrame:UnregisterEvent("WHO_LIST_UPDATE")
         C_FriendList.SendWho(zone)
         try = try + 1
-        WBC:Log('Scanning ' .. zone .. '...')
+        WBCoalition:Log('Scanning ' .. zone .. '...')
         C_Timer.After(3, function()
             if gotResultsFor < zoneIndex then
                 scanNextZone()
             end
         end)
     end,
-    OnCancel = function() isScanning = false end,
+    OnCancel = function()
+        isScanning = false
+        if zoneIndex > 1 then
+            reportResults()
+        end
+    end,
     timeout = 0,
     whileDead = true,
     hideOnEscape = true,
@@ -139,21 +152,20 @@ StaticPopupDialogs[DIALOG_CONTINUE_SCANNING] = {
 
 function Scanner:OnWhoResult()
     if not isScanning then return end
+    FriendsFrame:RegisterEvent("WHO_LIST_UPDATE")
     saveWhoResults()
     local zone = ZONES[zoneIndex]
-    try = 0
+    try = 1
     gotResultsFor = zoneIndex
     zoneIndex = zoneIndex + 1
     if zoneIndex > #ZONES then
-        WBC:Log('Scan completed')
+        isScanning = false
         reportResults()
     end
     scanNextZone()
 end
 
 function Scanner:Scan()
-    WBC = WBCoalition
-    WBC:Log('Scan initiated')
     results = {}
     isScanning = true
     zoneIndex = 1
